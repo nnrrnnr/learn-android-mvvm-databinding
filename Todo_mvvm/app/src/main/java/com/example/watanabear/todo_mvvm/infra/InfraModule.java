6f6b7b7efd9c718c548edab553a0dd53836631d1
Ryo Watanabe <watanabear@Ryo-no-MacBook-Air.local>
2017-03-18 23:55:37 +0900

    add asset

--- Todo_mvvm/app/src/main/java/com/example/watanabear/todo_mvvm/infra/InfraModule.java
+++ Todo_mvvm/app/src/main/java/com/example/watanabear/todo_mvvm/infra/InfraModule.java
@@ -1,6 +1,11 @@
 package com.example.watanabear.todo_mvvm.infra;
 
 
+import com.example.watanabear.todo_mvvm.infra.repository.TasksRepository;
+import com.example.watanabear.todo_mvvm.infra.repository.TasksRepositoryImpl;
+
+import javax.inject.Singleton;
+
 import dagger.Module;
 import dagger.Provides;
 
@@ -10,5 +15,9 @@ import dagger.Provides;
 @Module
 public class InfraModule {
 
-
+    @Singleton
+    @Provides
+    public TasksRepository provideTasksRepository() {
+        return new TasksRepositoryImpl();
+    }
 }
---
+++ Todo_mvvm/app/src/main/java/com/example/watanabear/todo_mvvm/presentation/tasks/TasksActivity.java
@@ -0,0 +1,15 @@
+package com.example.watanabear.todo_mvvm.presentation.tasks;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.watanabear.todo_mvvm.R;
+
+public class TasksActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tasks);
+    }
+}
