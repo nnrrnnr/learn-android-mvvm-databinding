eb55e28d2395bdf7346e870cd215b90ec5c83cd7
watanabear <watana6ear@gmail.com>
2017-03-20 18:34:42 +0900

    tasks incomplete

---
+++ Tasks/app/src/main/java/com/example/watanabear/tasks/presentation/tasks/TaskItemNavigator.java
@@ -0,0 +1,10 @@
+package com.example.watanabear.tasks.presentation.tasks;
+
+/**
+ * Created by watanabear on 2017/03/20.
+ */
+
+public interface TaskItemNavigator {
+
+    void onTaskClicked(String taskId);
+}
--- Tasks/app/src/main/java/com/example/watanabear/tasks/presentation/tasks/TaskItemViewModel.java
+++ Tasks/app/src/main/java/com/example/watanabear/tasks/presentation/tasks/TaskItemViewModel.java
@@ -1,6 +1,7 @@
 package com.example.watanabear.tasks.presentation.tasks;
 
 import android.databinding.BaseObservable;
+import android.databinding.Bindable;
 import android.databinding.ObservableField;
 
 import com.example.watanabear.tasks.domain.model.Task;
@@ -19,9 +20,34 @@ public class TaskItemViewModel extends BaseObservable {
     private final ObservableField<Task> mTaskObservable = new ObservableField<>();
 
     private final TasksRepository mTasksRepository;
+    private final TaskItemNavigator navigator;
 
-    public TaskItemViewModel(TasksRepository repository) {
+    public TaskItemViewModel(TasksRepository repository, TaskItemNavigator navigator) {
         this.mTasksRepository = repository;
+        this.navigator = navigator;
+    }
+
+    @Bindable
+    public boolean getCompleted() {
+        return mTaskObservable.get().isCompleted();
+    }
+
+    // This could be an observable, but we save a call to Task.getTitleForList() if not needed.
+    @Bindable
+    public String getTitleForList() {
+        if (mTaskObservable.get() == null) {
+            return "No data";
+        }
+        return mTaskObservable.get().getTitleForList();
+    }
+
+    public void taskClicked() {
+        String taskId = mTaskObservable.get().id;
+        if (taskId == null) {
+            // Click happened before task was loaded, no-op.
+            return;
+        }
+        navigator.onTaskClicked(taskId);
     }
 
 
